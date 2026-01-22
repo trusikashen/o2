2f10ef5cfeb1be6fc57fb662fa7c383069160a40
evagev2016-pixel <samyunwanarmenia@gmail.com>
2026-01-22 05:06:15 +0400

    restore project state from commit 42b179f

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a9672798928fb02a"; }
+/******/ 		__webpack_require__.h = function() { return "719acbf9e795b9b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
